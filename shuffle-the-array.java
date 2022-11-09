0f696efb05a609db5285e6bd2d1d5a2d508b212f
SujanBoopathy <sujansaradha@gmail.com>
2022-11-09 18:58:52 +0530

    problem-solving

---
+++ shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result=new int[2*n];
+        int i=0,j=n,k=0;
+        while(i<n){
+            result[k++]=nums[i++];
+            result[k++]=nums[j++];
+        }
+        return result;
+    }
+}
